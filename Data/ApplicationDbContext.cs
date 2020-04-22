d575d8182d31cc0e85e4e87f47a90eab7deb27a3
changdep96 <54663418+changdep96@users.noreply.github.com>
2020-04-22 10:19:43 +0700

    mới sửa

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,18 +1,17 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Reflection.Metadata;
 using Microsoft.EntityFrameworkCore;
 using ProjectManage.Models;
 
 namespace ProjectManage.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
 
-        // public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<Status> Statuses { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<UserProject> UserProject { get; set; }
@@ -22,30 +21,18 @@ namespace ProjectManage.Data
         override protected void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            
-            //========================================================================
 
-            // Tên bảng cho các bảng identity
-            builder.Entity<ApplicationUser>().ToTable("Users");
-            builder.Entity<Role>().ToTable("Roles");
-            builder.Entity<IdentityUserToken<int>>().ToTable("UserTokens");
-            builder.Entity<IdentityRoleClaim<int>>().ToTable("RoleClaims");
-            builder.Entity<UserRole>().ToTable("UserRoles");
-            builder.Entity<IdentityUserClaim<int>>().ToTable("UserClaims");
-            builder.Entity<IdentityUserLogin<int>>().ToTable("UserLogins");
-
-            //========================================================================
-            // 
-
-            //=== 1 - 1: User - Project <managerId>
-
-            builder.Entity<ApplicationUser>()
-                .HasOne(e => e.Project)
-                .WithOne(c => c.User)
-                .HasForeignKey<Project>(b => b.ManagerId)
-                .OnDelete(DeleteBehavior.Cascade);
+            //=== 1 - n: User - Project <managerId>
 
 
+         builder.Entity<User>()
+         .HasMany(p=>p.Projects)
+         .WithOne(u=>u.Users)
+         .HasForeignKey(p=>p.ManagerId)
+         .OnDelete(DeleteBehavior.Cascade);
+        
+
+ 
             //=== 1 - n: Project - ListTask
 
             builder.Entity<ListTask>()
@@ -76,22 +63,19 @@ namespace ProjectManage.Data
 
             builder.Entity<UserProject>()
                 .HasKey(bc => new { bc.UserId, bc.ProjectId });
-                
             builder.Entity<UserProject>()
                 .HasOne(bc => bc.User)
                 .WithMany(b => b.UserProjects)
                 .HasForeignKey(bc => bc.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
-
+                
 
             builder.Entity<UserProject>()
                 .HasOne(bc => bc.Project)
                 .WithMany(c => c.UserProjects)
                 .HasForeignKey(bc => bc.ProjectId)
                 .OnDelete(DeleteBehavior.Cascade);
-
-
-
+                
         }
     }
 
